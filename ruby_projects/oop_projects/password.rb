15cedfcc5bbc198119372abb9934177abbba1293
knewman23 <krys@trainual.com>
2022-06-25 15:44:50 -0700

    bcrypt users

--- ruby_projects/oop_projects/password.rb
+++ ruby_projects/oop_projects/password.rb
@@ -1,15 +1,33 @@
+require "bundler/inline"
+
+gemfile true do
+  source "http://rubygems.org"
+  gem "bcrypt"
+end
+
 require "bcrypt"
 
-my_password = BCrypt::Password.create("my password")
-#=> "$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa"
+users = [
+    { username: "mashrur", password: "password1" },
+    { username: "jack", password: "password2" },
+    { username: "arya", password: "password3" },
+    { username: "jonshow", password: "password4" },
+    { username: "heisenberg", password: "password5" }
+]
 
-puts my_password
-my_password.version              #=> "2a"
-my_password.cost                 #=> 10
-puts my_password == "my password"     #=> true
-puts my_password == "not my password" #=> false
+def create_hash_digest(password)
+  BCrypt::Password.create(password)
+end
 
+def verify_hash_digest(password)
+  BCrypt::Password.new(password)
+end
 
-# my_password = BCrypt::Password.new("$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa")
-# my_password == "my password"     #=> true
-# my_password == "not my password" #=> false
+def create_secure_users(list_of_users)
+  list_of_users.each do |user_record|
+    user_record[:password] = create_hash_digest(user_record[:password])
+  end
+  list_of_users
+end
+
+puts create_secure_users(users)
